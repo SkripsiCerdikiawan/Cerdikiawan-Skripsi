d8f300daa3ff828122dd9d95afecc510f3f5d4a2
NJuGau <nathanael.gauthama@binus.ac.id>
2024-09-29 16:13:30 +0700

    Initial Project initialization, xcodegen integration

---
+++ Cerdikiawan/Cerdikiawan/CerdikiawanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerdikiawanApp.swift
+//  Cerdikiawan
+//
+//  Created by Nathanael Juan Gauthama on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CerdikiawanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cerdikiawan/Cerdikiawan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cerdikiawan
+//
+//  Created by Nathanael Juan Gauthama on 29/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
