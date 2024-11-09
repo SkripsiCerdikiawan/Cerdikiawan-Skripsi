0a31d8fae528671e95005e8536f0182926614a14
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-09 14:26:24 +0700

    Doc: Add test environment, launch screen, and update yaml

---
+++ Cerdikiawan/CerdikiawanTests/CerdikiawanTests.swift
@@ -0,0 +1,16 @@
+//
+//  CerdikiawanTests.swift
+//  CerdikiawanTests
+//
+//  Created by Nathanael Juan Gauthama on 09/11/24.
+//
+
+import Testing
+
+struct CerdikiawanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
