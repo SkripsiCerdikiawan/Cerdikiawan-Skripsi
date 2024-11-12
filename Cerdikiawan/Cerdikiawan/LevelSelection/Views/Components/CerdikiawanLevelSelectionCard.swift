db5cc8b68bb893227c9202388e315f56ca04fd9f
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-12 16:55:48 +0700

    Create Level Selection Card Component

---
+++ Cerdikiawan/Cerdikiawan/LevelSelection/Views/Components/CerdikiawanLevelSelectionCard.swift
@@ -0,0 +1,23 @@
+//
+//  CerdikiawanLevelSelectionCard.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 12/11/24.
+//
+
+import SwiftUI
+
+struct CerdikiawanLevelSelectionCard: View {
+    var imageName: String
+    var title: String
+    var description: String
+    var availableBalanceToGain: Int
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CerdikiawanLevelSelectionCard()
+}
