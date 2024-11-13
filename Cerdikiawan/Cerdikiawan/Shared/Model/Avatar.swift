546ac047f46649b9edff095c6046c40ed7285287
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-13 15:31:50 +0700

    import: Cerdikiawan Avatars

---
+++ Cerdikiawan/Cerdikiawan/Shared/Model/Avatar.swift
@@ -0,0 +1,12 @@
+//
+//  Avatar.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 13/11/24.
+//
+
+struct Avatar {
+    let id: String
+    let name: String
+    let imageName: String
+}
