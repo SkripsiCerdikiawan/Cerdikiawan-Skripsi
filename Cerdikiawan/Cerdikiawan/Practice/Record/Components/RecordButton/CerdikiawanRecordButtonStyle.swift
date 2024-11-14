2c69cac890cffd7ed4790c1f04a89aeedb241d00
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-14 12:13:12 +0700

    feat: Create Cerdikiawan Record Button Component

---
+++ Cerdikiawan/Cerdikiawan/Practice/Record/Components/RecordButton/CerdikiawanRecordButtonStyle.swift
@@ -0,0 +1,74 @@
+//
+//  CerdikiawanRecordButtonState.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 14/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum CerdikiawanRecordButtonStyle {
+    case normal
+    case recording
+    case pause
+    case replay
+    case rerecord
+    
+    var text: String {
+        switch self {
+        case .normal:
+            return "Rekam"
+        case .recording:
+            return "Hentikan Rekaman"
+        case .pause:
+            return "Berhenti Mendengarkan"
+        case .replay:
+            return "Dengarkan Rekaman"
+        case .rerecord:
+            return "Rekam Ulang"
+        }
+    }
+    
+    var borderRadius: CGFloat {
+        return 8
+    }
+    
+    var foregroundStyle: Color {
+        return Color(.cWhite)
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .normal:
+            return Color(.cDarkBlue)
+        case .recording:
+            return Color(.cDarkRed)
+        case .pause:
+            return Color(.cDarkOrange)
+        case .replay:
+            return Color(.cLightBlue)
+        case .rerecord:
+            return Color(.cOrange)
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .normal:
+            return "microphone.fill"
+        case .recording:
+            return "stop.fill"
+        case .pause:
+            return "pause.fill"
+        case .replay:
+            return "play.fill"
+        case .rerecord:
+            return "microphone.fill"
+        }
+    }
+    
+    var fontColor: Color {
+        return Color(.secondaryLabel)
+    }
+}
