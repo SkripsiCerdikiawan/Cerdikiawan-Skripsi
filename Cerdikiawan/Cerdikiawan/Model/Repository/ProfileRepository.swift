ae79e864ca1e0eb3e51bca073b6e865c1941517e
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-17 17:27:01 +0700

    Feat: Add and implement test for update and delete profile

--- Cerdikiawan/Cerdikiawan/Model/Repository/ProfileRepository.swift
+++ Cerdikiawan/Cerdikiawan/Model/Repository/ProfileRepository.swift
@@ -20,28 +20,33 @@ class SupabaseProfileRepository: SupabaseRepository, ProfileRepository {
     private override init() {}
     
     func fetchProfile(request: ProfileFetchRequest) async throws -> (SupabaseProfile?, ErrorStatus) {
-        let response = try await client
-            .from("Profile")
-            .select()
-            .eq("profileId", value: request.profileId)
-            .single()
-            .execute()
-        
-        guard response.status == 200 else {
-            return (nil, .serverError)
-        }
-        
-        let result = JsonManager.shared.loadJSONData(from: response.data, as: SupabaseProfile.self)
-        
-        switch result {
-            case .success(let profile):
-            return (profile, .success)
-            case .failure(_):
-                return (nil, .jsonError)
+        do {
+            let response = try await client
+                .from("Profile")
+                .select()
+                .eq("profileId", value: request.profileId)
+                .single()
+                .execute()
+            
+            guard response.status == 200 else {
+                return (nil, .serverError)
+            }
+            
+            let result = JsonManager.shared.loadJSONData(from: response.data, as: SupabaseProfile.self)
+            
+            switch result {
+                case .success(let profile):
+                return (profile, .success)
+                case .failure(_):
+                    return (nil, .jsonError)
+            }
+        } catch {
+            return (nil, .notFound)
         }
     }
     
     func createNewProfile(request: ProfileInsertRequest) async throws -> (SupabaseProfile?, ErrorStatus) {
+        
         let profile = SupabaseProfile(profileId: request.profileId,
                                       profileName: request.profileName,
                                       profileBalance: request.profileBalance,
@@ -65,10 +70,65 @@ class SupabaseProfileRepository: SupabaseRepository, ProfileRepository {
     }
     
     func updateProfile(request: ProfileUpdateRequest) async throws -> (SupabaseProfile?, ErrorStatus) {
-        return (nil, .notFound)
+        let (profile, status) = try await fetchProfile(request: ProfileFetchRequest(profileId: request.profileId))
+        
+        guard status == .success, let initialProfile = profile else {
+            return (nil, .notFound)
+        }
+        
+        guard request.profileName != nil || request.profileBalance != nil || request.profileBirthDate != nil else {
+            return (profile, .invalidInput)
+        }
+        
+        var savedProfile = initialProfile
+        do {
+            if let profileName = request.profileName, profileName != savedProfile.profileName {
+                savedProfile = try await updateProfileData(updatedColumn: "profileName", updatedValue: profileName, profileId: request.profileId)
+            }
+            
+            if let profileBalance = request.profileBalance, profileBalance != savedProfile.profileBalance {
+                savedProfile = try await updateProfileData(updatedColumn: "profileBalance", updatedValue: profileBalance, profileId: request.profileId)
+            }
+            
+            if let profileBirthDate = request.profileBirthDate, profileBirthDate != savedProfile.profileBirthDate {
+                savedProfile = try await updateProfileData(updatedColumn: "profileBirthDate", updatedValue: profileBirthDate, profileId: request.profileId)
+            }
+            
+            return (savedProfile, .success)
+        } catch {
+            return (savedProfile, .serverError)
+        }
+    }
+    
+    private func updateProfileData<T: Encodable>(updatedColumn: String, updatedValue: T, profileId: UUID) async throws -> SupabaseProfile {
+        return try await client
+            .from("Profile")
+            .update([updatedColumn: updatedValue])
+            .eq("profileId", value: profileId)
+            .single()
+            .execute()
+            .value
     }
     
     func deleteProfile(request: ProfileDeleteRequest) async throws -> (ErrorStatus) {
-        return (.serverError)
-    }    
+        let (profile, status) = try await fetchProfile(request: ProfileFetchRequest(profileId: request.profileId))
+        
+        guard let toBeDeletedProfile = profile, status == .success else {
+            return (.notFound)
+        }
+        
+        do {
+            let response = try await client
+                .from("Profile")
+                .delete()
+                .eq("profileId", value: request.profileId)
+                .execute()
+            guard response.status == 200 else {
+                return (.serverError)
+            }
+            return (.success)
+        } catch {
+            return (.serverError)
+        }
+    }
 }
--- Cerdikiawan/CerdikiawanTests/ProfileTests.swift
+++ Cerdikiawan/CerdikiawanTests/ProfileTests.swift
@@ -16,7 +16,7 @@ struct ProfileTests {
         profileRepository = SupabaseProfileRepository.shared
     }
 
-    @Test func fetchProfile() async throws {
+    @Test func testFetchProfile() async throws {
         guard let profileId = UUID(uuidString: "2414c7e0-414c-4353-807f-200d009aace8") else {
             #expect(Bool(false), "Invalid profileId")
             return
@@ -28,15 +28,31 @@ struct ProfileTests {
         #expect(profile != nil, "Profile is nil")
     }
     
-    @Test func insertNewProfile() async throws {
-        guard let profileId = UUID(uuidString: "c96ea4c9-34a1-4c83-84f7-ad9e269de2f3") else {
+    @Test func testDeleteProfile() async throws {
+        guard let profileId = UUID(uuidString: "d7e08c6a-e80e-4288-9675-69dd454c14d8") else {
+            #expect(Bool(false), "Invalid profileId")
+            return
+        }
+        let deleteRequest = ProfileDeleteRequest(profileId: profileId)
+        let (deleteStatus) = try await profileRepository.deleteProfile(request: deleteRequest)
+        
+        #expect(deleteStatus == .success, "Failed to delete profile")
+        
+        let fetchRequest = ProfileFetchRequest(profileId: profileId)
+        let (profile, fetchStatus) = try await profileRepository.fetchProfile(request: fetchRequest)
+        
+        #expect(profile == nil && fetchStatus == .notFound, "Profile still exists")
+    }
+    
+    @Test func testInsertNewProfile() async throws {
+        guard let profileId = UUID(uuidString: "d7e08c6a-e80e-4288-9675-69dd454c14d8") else {
             #expect(Bool(false), "Invalid profileId")
             return
         }
         let request = ProfileInsertRequest(profileId: profileId,
-                                           profileName: "Hans",
+                                           profileName: "Rizky",
                                            profileBalance: 0,
-                                           profileBirthDate: "2003-12-12"
+                                           profileBirthDate: "2006-04-10"
         )
         
         let (profile, status) = try await profileRepository.createNewProfile(request: request)
@@ -45,4 +61,65 @@ struct ProfileTests {
         #expect(profile != nil, "Profile is nil")
     }
     
+    @Test func testUpdateProfileSuccess() async throws {
+        guard let profileId = UUID(uuidString: "c96ea4c9-34a1-4c83-84f7-ad9e269de2f3") else {
+            #expect(Bool(false), "Invalid profileId")
+            return
+        }
+        let request = ProfileUpdateRequest(profileId: profileId,
+                                           profileName: "Hans Cupiterson",
+                                           profileBalance: 10,
+                                           profileBirthDate: "2003-12-13"
+        )
+        
+        let (profile, status) = try await profileRepository.updateProfile(request: request)
+        
+        #expect(status == .success, "Unexpected status: \(status)")
+        #expect(profile != nil, "Profile should not be nil")
+        
+        if let updatedProfile = profile, status == .success {
+            #expect(updatedProfile.profileId == request.profileId, "Invalid profileId")
+            if request.profileName != nil {
+                #expect(updatedProfile.profileName == request.profileName, "Profile name did not updated")
+            }
+            if request.profileBalance != nil {
+                #expect(updatedProfile.profileBalance == request.profileBalance, "Profile balance did not updated")
+            }
+            if request.profileBirthDate != nil {
+                #expect(updatedProfile.profileBirthDate == request.profileBirthDate, "Profile birth date did not updated")
+            }
+        } else {
+            #expect(Bool(false), "Profile should not be nil")
+        }
+    }
+    
+    @Test func testUpdateProfileOneAttribute() async throws {
+        guard let profileId = UUID(uuidString: "9ebd74db-a0f0-4338-841a-36db80fa6aa7") else {
+            #expect(Bool(false), "Invalid profileId")
+            return
+        }
+        let request = ProfileUpdateRequest(profileId: profileId,
+                                           profileBalance: 99
+        )
+        
+        let (profile, status) = try await profileRepository.updateProfile(request: request)
+        
+        #expect(status == .success, "Unexpected status: \(status)")
+        #expect(profile != nil, "Profile should not be nil")
+        
+        if let updatedProfile = profile, status == .success {
+            #expect(updatedProfile.profileId == request.profileId, "Invalid profileId")
+            if request.profileName != nil {
+                #expect(updatedProfile.profileName == request.profileName, "Profile name did not updated")
+            }
+            if request.profileBalance != nil {
+                #expect(updatedProfile.profileBalance == request.profileBalance, "Profile balance did not updated")
+            }
+            if request.profileBirthDate != nil {
+                #expect(updatedProfile.profileBirthDate == request.profileBirthDate, "Profile birth date did not updated")
+            }
+        } else {
+            #expect(Bool(false), "Profile should not be nil")
+        }
+    }
 }
