90bb42958eaff14caa41326f418deea5cd521677
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-17 14:12:18 +0700

    Feat: Add and implement test for character repository

---
+++ Cerdikiawan/Cerdikiawan/Model/Request/CharacterRequest.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterRequest.swift
+//  Cerdikiawan
+//
+//  Created by Nathanael Juan Gauthama on 17/11/24.
+//
+
+import Foundation
+
+struct CharacterRequest {
+    var characterId: UUID? = nil
+}
