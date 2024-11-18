ff32578c97e47f9080ca7d24bdfee85cb41c6be6
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-18 16:24:51 +0700

    feat: Added Word Match Entity

---
+++ Cerdikiawan/Cerdikiawan/Practice/WordMatch/Models/WordMatchTextEntity.swift
@@ -0,0 +1,11 @@
+//
+//  WordMatchTextEntity.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 18/11/24.
+//
+
+struct WordMatchTextEntity: Hashable {
+    var id: String
+    var content: String
+}
---
+++ Cerdikiawan/Cerdikiawan/Practice/WordMatch/Views/Components/Container/CerdikiawanWordMatchContainer.swift
@@ -0,0 +1,18 @@
+//
+//  CerdikiawanWordMatchContainer.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 18/11/24.
+//
+
+import SwiftUI
+
+struct CerdikiawanWordMatchContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CerdikiawanWordMatchContainer()
+}
