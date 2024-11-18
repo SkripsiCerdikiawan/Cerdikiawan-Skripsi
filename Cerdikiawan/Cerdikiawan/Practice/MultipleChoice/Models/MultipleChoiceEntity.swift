f370babbfc3cb148db517d23ca87f192c9460ac5
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-18 08:47:05 +0700

    feat: Create Cerdikiawan Multiple Choice Container

---
+++ Cerdikiawan/Cerdikiawan/Practice/MultipleChoice/Models/MultipleChoiceEntity.swift
@@ -0,0 +1,28 @@
+//
+//  MultipleChoiceEntity.swift
+//  Cerdikiawan
+//
+//  Created by Hans Arthur Cupiterson on 17/11/24.
+//
+
+import Foundation
+
+struct MultipleChoiceEntity {
+    var id: String
+    var question: String
+    var answer: [MultipleChoiceAnswerEntity]
+    var correctAnswerID: String
+}
+
+extension MultipleChoiceEntity {
+    static func mock() -> [Self] {
+        return [
+            .init(
+                id: "mc-ques-01",
+                question: "This is an Example Question. Do you understand?",
+                answer: MultipleChoiceAnswerEntity.mock(),
+                correctAnswerID: "mc-answer-01"
+            )
+        ]
+    }
+}
