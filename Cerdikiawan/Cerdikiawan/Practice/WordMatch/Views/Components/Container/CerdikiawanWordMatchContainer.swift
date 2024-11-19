8563e28dd1ee9ca0a0b901785d82ad4e9ca918f6
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-11-19 15:45:24 +0700

    feat: Completing Logic to Swap Answer

--- Cerdikiawan/Cerdikiawan/Practice/WordMatch/Views/Components/Container/CerdikiawanWordMatchContainer.swift
+++ Cerdikiawan/Cerdikiawan/Practice/WordMatch/Views/Components/Container/CerdikiawanWordMatchContainer.swift
@@ -40,15 +40,71 @@ struct CerdikiawanWordMatchContainer: View {
                             label: pair[question.id]?.content ?? "",
                             type: determineType(questionID: question.id),
                             onTap: {
-                                if let selectedAnswer = selectedAnswer {
-                                    pair[question.id] = selectedAnswer
+                                // if question has answer value
+                                if let answer = pair[question.id] {
+                                    guard let selectedAnswer = selectedAnswer else {
+                                        self.selectedAnswer = answer
+                                        return
+                                    }
+                                    
+                                    // If user select answer again, deselect
+                                    if selectedAnswer.id == answer.id {
+                                        self.selectedAnswer = nil
+                                        return
+                                    }
+                                    
+                                    // Put Answer
+                                    guard let answer = pair[question.id] else {
+                                        return
+                                    }
+                                    
+                                    // Replace if answer is already put into pair
+                                    if let oldQuestion = pair.first(where: {
+                                        $1.id == selectedAnswer.id
+                                    })?.key {
+                                        pair[question.id] = selectedAnswer
+                                        pair[oldQuestion] = answer
+                                    }
+                                    else {
+                                        // Replace to answer pool if not
+                                        pair[question.id] = selectedAnswer
+                                        answerPool.append(answer)
+                                        
+                                        // Swap the answer position
+                                        if let firstIndex = answerPool.firstIndex(of: selectedAnswer),
+                                           let secondIndex = answerPool.firstIndex(of: answer) {
+                                            answerPool.swapAt(firstIndex, secondIndex)
+                                        }
+                                        
+                                        answerPool.removeAll(where: { $0.id == selectedAnswer.id })
+                                        
+                                    }
+                                    
+                                    
                                     self.selectedAnswer = nil
-                                    answerPool.removeAll(where: { $0.id == selectedAnswer.id})
+                                    
                                 }
-                                else if let answer = pair[question.id] {
-                                    answerPool.append(answer)
-                                    pair[question.id] = nil
+                                // If not, put answer
+                                else {
+                                    guard let selectedAnswer = selectedAnswer else {
+                                        return
+                                    }
+                                    
+                                    // Check if selected answer is already inserted
+                                    if let oldQuestion = pair.first(where: {
+                                        $1.id == selectedAnswer.id
+                                    })?.key {
+                                        pair[oldQuestion] = nil
+                                        pair[question.id] = selectedAnswer
+                                    }
+                                    else {
+                                        pair[question.id] = selectedAnswer
+                                        answerPool.removeAll(where: { $0.id == selectedAnswer.id })
+                                    }
+                                    
+                                    self.selectedAnswer = nil
                                 }
+                                
                             }
                         )
                     }
@@ -67,19 +123,47 @@ struct CerdikiawanWordMatchContainer: View {
                             label: answer.content,
                             type: determineType(answerID: answer.id),
                             onTap: {
+                                // If there's no selected answer, set selected answer
                                 if selectedAnswer == nil {
-                                    debugPrint("Selected Answer: \(answer.content)")
                                     selectedAnswer = answer
                                 }
                                 else {
-                                    guard let selected = selectedAnswer,
-                                          selected.id != answer.id else {
-                                        debugPrint("Selected Answer: nil")
-                                        selectedAnswer = nil
-                                        return
+                                    // if any selected answer
+                                    if let selectedAnswer = selectedAnswer {
+                                        // If selected answer is pressed again. unselect it
+                                        if selectedAnswer.id == answer.id {
+                                            self.selectedAnswer = nil
+                                        }
+                                        else {
+                                            // If selected answer is from the answer pool, swap it
+                                            if let firstIndex = answerPool.firstIndex(of: selectedAnswer),
+                                               let secondIndex = answerPool.firstIndex(of: answer) {
+                                                answerPool.swapAt(firstIndex, secondIndex)
+                                            }
+                                            // If selected answer is from pair, swap it
+                                            else {
+                                                if let question = pair.first(where: {
+                                                    $1.id == selectedAnswer.id
+                                                })?.key,
+                                                    let pairAnswer = pair[question] {
+                                                    answerPool.append(pairAnswer)
+                                                    
+                                                    // Swap the answer position
+                                                    if let firstIndex = answerPool.firstIndex(of: pairAnswer),
+                                                       let secondIndex = answerPool.firstIndex(of: answer) {
+                                                        answerPool.swapAt(firstIndex, secondIndex)
+                                                    }
+                                                    
+                                                    pair[question] = answer
+                                                    answerPool.removeAll(where: { $0.id == answer.id})
+                                                    
+                                                }
+                                            }
+                                            self.selectedAnswer = nil
+                                        }
                                     }
                                     
-                                    selectedAnswer = answer
+                                    
                                 }
                             }
                         )
@@ -107,11 +191,15 @@ struct CerdikiawanWordMatchContainer: View {
     private func determineType(questionID: String) -> CerdikiawanWordMatchTextContainerType {
         switch state {
         case .answering:
-            guard pair[questionID] != nil else {
+            guard let answer = pair[questionID] else {
                 if selectedAnswer != nil {
                     return .blank
                 }
                 return .question
+            }
+            
+            if selectedAnswer == answer {
+                return .filled
             }
             return .answer
             
