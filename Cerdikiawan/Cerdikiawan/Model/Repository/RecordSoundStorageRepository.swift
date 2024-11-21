4f1d749e50f40c92148738b3295de9959612d607
NJuGau <nathanael.gauthama@binus.ac.id>
2024-11-22 01:07:47 +0700

    Feat: Add and implement test for sound record repository

---
+++ Cerdikiawan/Cerdikiawan/Model/Repository/RecordSoundStorageRepository.swift
@@ -0,0 +1,34 @@
+//
+//  SoundStorageRepository.swift
+//  Cerdikiawan
+//
+//  Created by Nathanael Juan Gauthama on 21/11/24.
+//
+
+import Foundation
+
+protocol RecordSoundStorageRepository {
+    func uploadSoundFile(request: RecordSoundRequest) async throws -> (SupabaseRecordSound?, ErrorStatus)
+    func downloadSound(request: RecordSoundRequest) async throws -> (SupabaseRecordSound?, ErrorStatus)
+}
+
+class SupabaseRecordSoundStorageRepository: StorageRepository, RecordSoundStorageRepository {
+    
+    public static let shared = SupabaseRecordSoundStorageRepository()
+    
+    func uploadSoundFile(request: RecordSoundRequest) async throws -> (SupabaseRecordSound?, ErrorStatus) {
+        let filePath = "\(request.userId)/\(request.attemptId).mp3"
+        try await client.from("RecordSound").upload(filePath, data: request.soundData)
+        
+        return (SupabaseRecordSound(soundPath: filePath, soundData: request.soundData), .success)
+    }
+    
+    func downloadSound(request: RecordSoundRequest) async throws -> (SupabaseRecordSound?, ErrorStatus) {
+        let filePath = "\(request.userId)/\(request.attemptId).mp3"
+        let data = try await client.from("RecordSound").download(path: filePath)
+        return (SupabaseRecordSound(soundPath: filePath, soundData: data), .success)
+    }
+    
+    
+}
+
